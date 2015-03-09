eeea2191e4d8bdfaf3326da7f3d85dc7fc34bfda
Tony Brix <tbrix13+github.com@gmail.com>
2015-03-09 16:16:26 -0500

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,113 @@
+window.storage = {
+	setItem: function (item, value) {
+		var args = arguments;
+		if (args.length > 2) {
+			var obj = storage.getItem(item) || {};
+			var next = obj;
+			for (var i = 1; i < args.length - 2; i++) {
+				if (!next.hasOwnProperty(args[i])) {
+					next[args[i]] = {};
+				}
+				next = next[args[i]];
+			}
+			next[args[args.length - 2]] = args[args.length - 1];
+			value = obj;
+		}
+		if (!storage.hasOwnProperty(item)) {
+			storage.define(item);
+		}
+		return localStorage.setItem(item, this.stringify(value));
+	},
+	getItem: function (item) {
+		return this.parse(localStorage.getItem(item));
+	},
+	removeItem: function (item) {
+		return localStorage.removeItem(item);
+	},
+	clear: function () {
+		return localStorage.clear();
+	},
+	key: function (key) {
+		return localStorage.key(key);
+	},
+	define: function (item) {
+		Object.defineProperty(window.storage, item, {
+			get: function () {
+				return storage.getItem(item);
+			}
+		});
+	},
+	stringify: function (obj) {
+		function ToString(obj, recursiveCall) {
+			//TODO: better type to string to reduce overlap with actual strings
+			//TODO: test circulation
+			var str;
+			if (typeof obj === "object") {
+				if (obj instanceof Date) {
+					str = "DATE:" + obj.toString();
+				} else if (obj instanceof RegExp) {
+					str = "REGEXP:" + obj.toString();
+				} else {
+					var temp = {};
+					for (var i in obj) {
+						temp[i] = ToString(obj[i], true);
+					}
+					if (!recursiveCall) {
+						str = JSON.stringify(temp);
+					} else {
+						str = temp;
+					}
+				}
+			} else if (typeof obj === "undefined") {
+				str = "UNDEFINED:";
+			} else if (typeof obj === "function") {
+				str = "FUNCTION:" + obj.toString();
+			} else if (!recursiveCall) {
+				str = JSON.stringify(obj);
+			} else {
+				str = obj;
+			}
+			return str;
+		}
+		return ToString(obj);
+	},
+	parse: function (str) {
+		//TODO: better detection of types
+		var obj = JSON.parse(str);
+		for (var i in obj) {
+			if (typeof obj[i] === "string") {
+				if (obj[i].indexOf("DATE:") === 0) {
+					var date = new Date(obj[i].substring(5));
+					if (!isNaN(date.getTime())) {
+						obj[i] = date;
+					}
+				} else if (obj[i].indexOf("REGEXP:") === 0) {
+					var matches = obj[i].match(/^REGEXP:\/((?:\\\/|[^/])+)\/([gimy]*)$/);
+					if (matches !== null) {
+						obj[i] = new RegExp(matches[1], matches[2]);
+					}
+				} else if (obj[i] === "UNDEFINED:") {
+					obj[i] = undefined;
+				} else if (obj[i].indexOf("FUNCTION:") === 0) {
+					var matches = obj[i].match(/^FUNCTION:function[^(]*\(([^)]*)\)\s*{([\s\S]*)}$/);
+					if (matches !== null) {
+						var args = matches[1].replace(/[\s/*]/g, "");
+						var body = matches[2].replace(/\r?\n|\r/g, "");
+						obj[i] = eval("new Function(\"" + args.split(", ").join("\", \"") + "\", \"" + body + "\")");
+					}
+				}
+			}
+		}
+		return obj;
+	}
+};
+Object.defineProperty(window.storage, "length", {
+	get: function () {
+		return localStorage.length;
+	}
+});
+for (var i in localStorage)
+{
+	storage.define(i);
+}
+//TODO: set storage.item = {test:""}; with object.observe?
