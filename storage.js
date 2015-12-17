c9b7517e6d23cbf61495ec2f01d556e142dbdb96
Tony Brix <tbrix13@uzitech.com>
2015-12-17 15:59:19 -0600

    Version 1.0.0
    fix binary
    add binary test
    add test.html

--- storage.js
+++ storage.js
@@ -1,3 +1,9 @@
+/*
+ * Author: Tony Brix, https://tony.brix.ninja
+ * License: MIT
+ * Version: 1.0.0
+ */
+;
 (function (window, undefined) {
 	window.storage = {};
 	Object.defineProperties(window.storage, {
@@ -5,7 +11,7 @@
 			value: function (item, value) {
 				var args = arguments;
 				if (args.length > 2) {
-					var obj = storage.getItem(item) || {};
+					var obj = window.storage.getItem(item) || {};
 					var next = obj;
 					if (next === null || typeof next !== "object") {
 						throw "'" + item + "' is not an object";
@@ -22,13 +28,13 @@
 					next[args[args.length - 2]] = args[args.length - 1];
 					value = obj;
 				}
-				storage.define(item);
-				return localStorage.setItem(item, storage.stringify(value));
+				window.storage.define(item);
+				return localStorage.setItem(item, window.storage.stringify(value));
 			}
 		},
 		getItem: {
 			value: function (item) {
-				return storage.parse(localStorage.getItem(item));
+				return window.storage.parse(localStorage.getItem(item));
 			}
 		},
 		removeItem: {
@@ -39,8 +45,8 @@
 		clear: {
 			value: function () {
 				var ret = localStorage.clear();
-				for (var i in storage) {
-					delete storage[i];
+				for (var i in window.storage) {
+					delete window.storage[i];
 				}
 				return ret;
 			}
@@ -52,14 +58,14 @@
 		},
 		define: {
 			value: function (item) {
-				if (!storage.hasOwnProperty(item)) {
+				if (!window.storage.hasOwnProperty(item)) {
 					Object.defineProperty(window.storage, item, {
 						enumerable: true,
 						get: function () {
-							return storage.getItem(item);
+							return window.storage.getItem(item);
 						},
 						set: function (value) {
-							return storage.setItem(item, value);
+							return window.storage.setItem(item, value);
 						}
 					});
 				}
@@ -138,13 +144,13 @@
 						return obj.hasOwnProperty('$binary') && Object.keys(obj).length === 1;
 					},
 					matchObject: function (obj) {
-						return typeof Uint8Array !== 'undefined' && obj instanceof Uint8Array || (obj && obj.hasOwnProperty('$Uint8ArrayPolyfill'));
+						return typeof window.Uint8Array !== 'undefined' && obj instanceof window.Uint8Array || (obj && obj.hasOwnProperty('$Uint8ArrayPolyfill'));
 					},
 					toJSONValue: function (obj) {
-						return {$binary: Base64.encode(obj)};
+						return {$binary: window.storage.base64.encode(obj)};
 					},
 					fromJSONValue: function (obj) {
-						return Base64.decode(obj.$binary);
+						return window.storage.base64.decode(obj.$binary);
 					}
 				},
 				undefined: {// Undefined
@@ -166,13 +172,13 @@
 						return obj.hasOwnProperty('$infnan') && Object.keys(obj).length === 1;
 					},
 					matchObject: function (obj) {
-						return (typeof obj === "number" && isNaN(obj)) || obj === Infinity || obj === -Infinity;
+						return (typeof obj === "number" && isNaN(obj)) || obj === window.Infinity || obj === -window.Infinity;
 					},
 					toJSONValue: function (obj) {
 						var sign;
-						if (obj === Infinity) {
+						if (obj === window.Infinity) {
 							sign = 1;
-						} else if (obj === -Infinity) {
+						} else if (obj === -window.Infinity) {
 							sign = -1;
 						} else {
 							sign = 0;
@@ -201,7 +207,7 @@
 						//var arr = args.split(",");
 						//arr.push(body);
 						//return Function.apply(null, arr);
-						
+
 						//allow named functions
 						return eval("(function(){ return function " + name + "(" + args + "){ " + body + " }; })()");
 					}
@@ -211,8 +217,8 @@
 						return obj.hasOwnProperty('$escape') && Object.keys(obj).length === 1;
 					},
 					matchObject: function (obj) {
-						for (var i in storage.converters) {
-							if (storage.converters[i].matchJSONValue(obj)) {
+						for (var i in window.storage.converters) {
+							if (window.storage.converters[i].matchJSONValue(obj)) {
 								return true;
 							}
 						}
@@ -221,14 +227,14 @@
 					toJSONValue: function (obj) {
 						var newObj = {};
 						for (var i in obj) {
-							newObj[i] = storage.stringify(obj[i]);
+							newObj[i] = window.storage.stringify(obj[i]);
 						}
 						return {$escape: newObj};
 					},
 					fromJSONValue: function (obj) {
 						var newObj = {};
 						for (var i in obj.$escape) {
-							newObj[i] = storage.parse(obj.$escape[i]);
+							newObj[i] = window.storage.parse(obj.$escape[i]);
 						}
 						return newObj;
 					}
@@ -238,14 +244,14 @@
 		stringify: {
 			value: function (obj) {
 				//check for circular reference.
-				if (storage.isCyclic(obj)) {
+				if (window.storage.isCyclic(obj)) {
 					throw "Cannot store cyclic objects";
 				}
 				function ToStringable(obj) {
 					if (obj !== null) {
-						for (var i in storage.converters) {
-							if (storage.converters[i].matchObject(obj)) {
-								return storage.converters[i].toJSONValue(obj);
+						for (var i in window.storage.converters) {
+							if (window.storage.converters[i].matchObject(obj)) {
+								return window.storage.converters[i].toJSONValue(obj);
 							}
 						}
 
@@ -274,9 +280,9 @@
 			value: function (str) {
 				function ToObject(obj) {
 					if (obj !== null && typeof obj === "object") {
-						for (var i in storage.converters) {
-							if (storage.converters[i].matchJSONValue(obj)) {
-								return storage.converters[i].fromJSONValue(obj);
+						for (var i in window.storage.converters) {
+							if (window.storage.converters[i].matchJSONValue(obj)) {
+								return window.storage.converters[i].fromJSONValue(obj);
 							}
 						}
 						if (obj instanceof Array) {
@@ -298,11 +304,139 @@
 			get: function () {
 				return localStorage.length;
 			}
+		},
+		base64: {
+			value: {
+				encode: function (array) {
+					return Base64.encode(array);
+				},
+				decode: function (str) {
+					return Base64.decode(str);
+				}
+			}
 		}
 	});
+
+	// modified from https://github.com/meteor/meteor/blob/devel/packages/base64/base64.js
+	// Base 64 encoding
+
+	var BASE_64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+	var BASE_64_VALS = {
+		"=": -1
+	};
+
+	for (var i = 0; i < BASE_64_CHARS.length; i++) {
+		BASE_64_VALS[BASE_64_CHARS.charAt(i)] = i;
+	}
+
+	var Base64 = {
+		encode: function (array) {
+			if (typeof array === "string") {
+				var str = array;
+				array = new Uint8Array(new ArrayBuffer(str.length));
+				for (var i = 0; i < str.length; i++) {
+					var ch = str.charCodeAt(i);
+					if (ch > 0xFF) {
+						throw new Error(
+								"Not ascii. Base64.encode can only take ascii strings.");
+					}
+					array[i] = ch;
+				}
+			}
+			var answer = [];
+			var a = null;
+			var b = null;
+			var c = null;
+			var d = null;
+			for (var i = 0; i < array.length; i++) {
+				switch (i % 3) {
+					case 0:
+						a = (array[i] >> 2) & 0x3F;
+						b = (array[i] & 0x03) << 4;
+						break;
+					case 1:
+						b = b | (array[i] >> 4) & 0xF;
+						c = (array[i] & 0xF) << 2;
+						break;
+					case 2:
+						c = c | (array[i] >> 6) & 0x03;
+						d = array[i] & 0x3F;
+						answer.push(BASE_64_CHARS.charAt(a));
+						answer.push(BASE_64_CHARS.charAt(b));
+						answer.push(BASE_64_CHARS.charAt(c));
+						answer.push(BASE_64_CHARS.charAt(d));
+						a = null;
+						b = null;
+						c = null;
+						d = null;
+						break;
+				}
+			}
+			if (a !== null) {
+				answer.push(BASE_64_CHARS.charAt(a));
+				answer.push(BASE_64_CHARS.charAt(b));
+				if (c === null)
+					answer.push('=');
+				else
+					answer.push(BASE_64_CHARS.charAt(c));
+				if (d === null)
+					answer.push('=');
+			}
+			return answer.join("");
+		},
+		decode: function (str) {
+			var len = Math.floor((str.length * 3) / 4);
+			if (str.charAt(str.length - 1) === '=') {
+				len--;
+				if (str.charAt(str.length - 2) === '=')
+					len--;
+			}
+			var arr = new Uint8Array(new ArrayBuffer(len));
+
+			var one = null;
+			var two = null;
+			var three = null;
+
+			var j = 0;
+
+			for (var i = 0; i < str.length; i++) {
+				var c = str.charAt(i);
+				var v = BASE_64_VALS[c];
+				switch (i % 4) {
+					case 0:
+						if (v < 0)
+							throw new Error('invalid base64 string');
+						one = v << 2;
+						break;
+					case 1:
+						if (v < 0)
+							throw new Error('invalid base64 string');
+						one = one | (v >> 4);
+						arr[j++] = one;
+						two = (v & 0x0F) << 4;
+						break;
+					case 2:
+						if (v >= 0) {
+							two = two | (v >> 2);
+							arr[j++] = two;
+							three = (v & 0x03) << 6;
+						}
+						break;
+					case 3:
+						if (v >= 0) {
+							arr[j++] = three | v;
+						}
+						break;
+				}
+			}
+			return arr;
+		}
+	};
+
 	for (var i in localStorage)
 	{
-		storage.define(i);
+		window.storage.define(i);
 	}
-//TODO: set storage.item = {test:""}; with object.observe?
+//TODO: set window.storage.item = {test:""}; with object.observe?
 })(window);
--- tests.js
+++ tests.js
@@ -1,12 +1,19 @@
 function test(name, value, pass) {
-	var stringify = storage.stringify(value);
-	var parse = storage.parse(stringify);
+	var stringify = window.storage.stringify(value);
+	var parse = window.storage.parse(stringify);
 	console.log("%c" + name.toUpperCase(), "color: #00f");
 	console.log("value: ", value);
 	console.log("stringify: ", stringify);
 	console.log("parse: ", parse);
 	if (typeof pass === "function") {
-		if (pass(value, stringify, parse)) {
+		var passing = false;
+		try {
+			passing = pass(value, stringify, parse);
+		} catch (ex) {
+			console.error(ex);
+			passing = false;
+		}
+		if (passing) {
 			console.log("%cPASS", "color: #0f0");
 		} else {
 			console.error("FAIL");
@@ -47,21 +54,17 @@ test("object", {1: 1, 2: 2, 3: 3}, function (test, string, result) {
 test("number", 1, function (test, string, result) {
 	return test === +string && test === result;
 });
-test("nan", NaN, function (test, string, result) {
+test("nan", window.NaN, function (test, string, result) {
 	return JSON.stringify({$infnan: 0}) === string && typeof result === "number" && isNaN(result);
 });
-test("infinity", Infinity, function (test, string, result) {
+test("infinity", window.Infinity, function (test, string, result) {
 	return JSON.stringify({$infnan: 1}) === string && test === result;
 });
-test("-infinity", -Infinity, function (test, string, result) {
+test("-infinity", -window.Infinity, function (test, string, result) {
 	return JSON.stringify({$infnan: -1}) === string && test === result;
 });
 test("date", new Date(), function (test, string, result) {
-	try {
-		return JSON.stringify({$date: test.getTime()}) === string && test.getTime() === result.getTime();
-	} catch (ex) {
-		return false;
-	}
+	return JSON.stringify({$date: test.getTime()}) === string && test.getTime() === result.getTime();
 });
 test("regexp", /i/g, function (test, string, result) {
 	return JSON.stringify({$regexp: test.toString()}) === string && test.toString() === result.toString();
@@ -75,32 +78,35 @@ test("named function", function diff_1(a, b) {
 	}
 	return a - b;
 }, function (test, string, result) {
-	try {
-		return test.name === result.name && JSON.stringify({$function: test.toString()}) === string && result(1, 3) === 2;
-	} catch (ex) {
-		return false;
-	}
+	return test.name === result.name && JSON.stringify({$function: test.toString()}) === string && result(1, 3) === 2;
 });
 test("anonymous function", function (a, b) {
 	return a + b;
 }, function (test, string, result) {
-	try {
-		return JSON.stringify({$function: test.toString()}) === string && result(1, 2) === 3;
-	} catch (ex) {
-		return false;
-	}
+	return JSON.stringify({$function: test.toString()}) === string && result(1, 2) === 3;
 });
 test("escape", {$date: 9271384}, function (test, string, result) {
-	try {
-		var newObj = {};
-		for (var i in test) {
-			newObj[i] = JSON.stringify(test[i]);
-		}
-		return JSON.stringify({$escape: newObj}) === string && test.$date === result.$date;
-	} catch (ex) {
-		return false;
+	var newObj = {};
+	for (var i in test) {
+		newObj[i] = JSON.stringify(test[i]);
 	}
+	return JSON.stringify({$escape: newObj}) === string && test.$date === result.$date;
 });
 test("null", null, function (test, string, result) {
 	return "null" === string && test === result;
+});
+test("binary", new TextEncoder("utf-8").encode("Hello"), function (test, string, result) {
+	var pass = true;
+	if (JSON.stringify({$binary: window.storage.base64.encode(test)}) !== string) {
+		pass = false;
+	} else if (test.length !== result.length || !result instanceof window.Uint8Array) {
+		pass = false;
+	} else {
+		for (var i = 0; i < test.length; i++) {
+			if (test[i] !== result[i]) {
+				pass = false;
+			}
+		}
+	}
+	return pass;
 });
